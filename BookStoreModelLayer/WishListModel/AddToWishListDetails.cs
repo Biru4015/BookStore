3b3f9b21cfef52dfde3b97235149812049355deb
Biru4015 <rambirendra12@gmail.com>
2020-07-25 19:38:33 +0530

    [Birendra]Add added. comment in WishList model classess.

--- BookStoreModelLayer/WishListModel/AddToWishListDetails.cs
+++ BookStoreModelLayer/WishListModel/AddToWishListDetails.cs
@@ -4,22 +4,46 @@ using System.Text;
 
 namespace BookStoreModelLayer.WishListModel
 {
+    /// <summary>
+    /// This class contains the code for add wish list details.
+    /// </summary>
     public class AddToWishListDetails
     {
+        /// <summary>
+        /// This  is wishlistid
+        /// </summary>
         public int WishListId { get; set; }
 
+        /// <summary>
+        /// This is user id.
+        /// </summary>
         public int UserId { get; set; }
 
+        /// <summary>
+        /// This is book id.
+        /// </summary>
         public int BookId { get; set; }
 
+        /// <summary>
+        /// This is book name.
+        /// </summary>
         public string BookName { get; set; }
 
         public string Catagory { get; set; }
 
+        /// <summary>
+        /// This is AuthorName.
+        /// </summary>
         public string AuthorName { get; set; }
 
+        /// <summary>
+        /// This is price of book.
+        /// </summary>
         public double Price { get; set; }
 
+        /// <summary>
+        /// This  is quantity of book.
+        /// </summary>
         public int Quantity { get; set; }
     }
 }
--- BookStoreModelLayer/WishListModel/CustomerWishListDetails.cs
+++ BookStoreModelLayer/WishListModel/CustomerWishListDetails.cs
@@ -5,38 +5,71 @@ using System.Text;
 
 namespace BookStoreModelLayer.WishListModel
 {
+    /// <summary>
+    /// This class contains the code for Customer WishList details.
+    /// </summary>
     public class CustomerWishListDetails
     {
+        /// <summary>
+        /// This is wishlist id.
+        /// </summary>
         [Required]
         public int WishListId { get; set; }
 
+        /// <summary>
+        /// This is bookId.
+        /// </summary>
         [Required]
         public int BookId { get; set; }
 
+        /// <summary>
+        /// This is BookName
+        /// </summary>
         [Required]
         public string BookName { get; set; }
 
+        /// <summary>
+        /// This is AuthorName
+        /// </summary>
         [Required]
         public string AuthorName { get; set; }
 
+        /// <summary>
+        /// This is price of book.
+        /// </summary>
         [Required]
         public double Price { get; set; }
 
+        /// <summary>
+        /// This  is book Quantity.
+        /// </summary>
         [Required]
         public int Quantity { get; set; }
 
         [Required]
         public bool IsMoved { get; set; }
 
+        /// <summary>
+        /// This is checking deleted details or not.
+        /// </summary>
         [Required]
         public bool IsDeleted { get; set; }
 
+        /// <summary>
+        /// This is date of creation.
+        /// </summary>
         [Required]
         public DateTime DateCreated { get; set; }
 
+        /// <summary>
+        /// This is date of modified
+        /// </summary>
         [Required]
         public DateTime DateModified { get; set; }
 
+        /// <summary>
+        /// This is BookImage
+        /// </summary>
         [Required]
         public string BookImage { get; set; }
     }
